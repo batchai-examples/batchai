6ab205541b6c26b5457a0d1b61f69177f00ac760
Yiting Qiang <qiangyt@wxcount.com>
2024-11-22 15:24:28 +0800

    changes by batchai "check"

--- pkg/check_report.go
+++ pkg/check_report.go
@@ -12,7 +12,7 @@ import (
 
 const CHECK_REPORT_JSON_FORMAT = `
 {
-  "has_issue": true or false,
+  "has_issue": true,
   "issues": [
     {
 	   "short_description": "...",
@@ -35,41 +35,23 @@ const CHECK_REPORT_JSON_FORMAT = `
        "severity_reason": "..."
     }
   ],
-  "overall_severity": "trivial" or "minor" or "major" or "critical"
+  "overall_severity": "minor"
 }`
 
-type CheckIssueT struct {
-	ShortDescription     string   `json:"short_description"`
-	DetailedExplaination string   `json:"detailed_explaination"`
-	Suggestion           string   `json:"suggestion"`
-	IssueLineBegin       int      `json:"issue_line_begin"`
-	IssueLineEnd         int      `json:"issue_line_end"`
-	IssueReferenceUrls   []string `json:"issue_reference_urls"`
-	Severity             string   `json:"severity"`
-	SeverityReason       string   `json:"severity_reason"`
-}
-
-type CheckIssue = *CheckIssueT
-
-func (me CheckIssue) Print(console comm.Console) {
-	console.NewLine().Printf("Short Description: %s", me.ShortDescription)
-	console.NewLine().Printf("Detailed Description: %s", me.DetailedExplaination)
-	console.NewLine().Printf("Severity: %s", me.Severity)
-	console.NewLine().Printf("Severity Reason: %s", me.SeverityReason)
-	console.NewLine().Printf("Suggestion: %s", me.Suggestion)
-	console.NewLine().Printf("IssueLineBegin: %d", me.IssueLineBegin)
-	console.NewLine().Printf("IssueLineEnd: %d", me.IssueLineEnd)
-	console.NewLine().Printf("IssueReferenceUrls: %s", me.IssueReferenceUrls)
-}
-
 type CheckReportT struct {
-	HasIssue          bool              `json:"has_issue"`
-	OverallSeverity   string            `json:"overall_severity"`
-	Issues            []CheckIssue      `json:"issues"`
-	FixedCode         string            `json:"fixed_code"`
-	OriginalCode      string            `json:"original_code"`
-	Path              string            `json:"path"`
-	ModelUsageMetrics ModelUsageMetrics `json:"model_usage_metrics"`
+	HasIssue                bool
+	Issues                  []struct {
+		ShortDescription        string
+		DetailedExplanation     string
+		Suggestion              string
+		IssueLineBegin          int
+		IssueLineEnd            int
+		IssueReferenceURLs    []string
+		Severity                string
+		SeverityReason          string
+	}
+	OverallSeverity         string
+	ModelUsageMetrics       ModelUsageMetrics
 }
 
 type CheckReport = *CheckReportT
